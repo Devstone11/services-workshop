2f8ebd39a550899de716f816794a997000f9d2ef
Devin Stone <devstone11@gmail.com>
2016-08-17 10:41:28 -0600

    complete exercise

--- controllers/post_message_controller.js
+++ controllers/post_message_controller.js
@@ -1,4 +1,9 @@
-app.controller('PostMessageController', function ($scope) {
+app.controller('PostMessageController', function ($scope, MessagesService, $location) {
   $scope.vw = {};
   $scope.vw.success = "Success!!"
+  $scope.vw.post = function(message) {
+    console.log(message);
+    MessagesService.add(message);
+    $location.url('/');
+  }
 })
